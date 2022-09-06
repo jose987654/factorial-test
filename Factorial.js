2a66d9cceaefb58999689b27cb862f690a3ea8e9
joseph <wasswajose9@gmail.com>
2022-09-06 12:24:50 +0000

    factorial

--- Factorial.js
+++ Factorial.js
@@ -1,6 +1,6 @@
 module.exports = function factorial(num) {
-    if (num === 0 || num === 1) return 1;
-    for (i = num - 1; i >= 1; i--) {      
+    if (num == 0 || num == 1) return 1;
+    for (i = num - 1; i >= 2; i--) {      
       num = num * i;  
     }
     return num;
